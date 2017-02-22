ad4b414e8ba78f20b989cee23e9b704745cd7b25
Ivan Azarkevich <postyly@yandex.ru>
2017-02-22 21:33:10 +0300

    commit 11 autodetection image size

--- views/image.view.php
+++ views/image.view.php
@@ -1,6 +1,12 @@
+<?php
+//получим размер загружаемого изображения
+$size = getimagesize("image/{$path}.png");
+$width = $size[0];
+$height = $size[1];
+?>
 <div>
     <h3 class="text-center"><?= $title_img ?></h3>
-    <img class="center-block" src="image/<?= $path ?>.png">
+    <img class="center-block" src="image/<?= $path ?>.png" width="<?= $width ?>" height="<?= $height ?>">
 </div>
 
 <!--результат вычисления-->
